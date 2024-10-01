0659d1489157cce4bd30eea266984aece1145072
Isurika00 <isurikaeshani@gmail.com>
2024-10-01 22:38:18 +0530

    add user screens

---
+++ lib/user_screen/trip_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TripDetails extends StatefulWidget {
+  const TripDetails({super.key, required String category});
+
+  @override
+  State<TripDetails> createState() => _TripDetailsState();
+}
+
+class _TripDetailsState extends State<TripDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
